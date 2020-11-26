949ccacb9be2dd8f6a51664c8e22f6567be61830
rpshende <rpshende@DESKTOP-VK57T7M>
2020-11-26 18:07:19 -0500

    commit

--- users/urls.py
+++ users/urls.py
@@ -1,12 +1,12 @@
 """Defines URL patterns for `users` app."""
-from typing import List
+from typing import List, Union
 from django.urls import path, include
 from django.urls.resolvers import URLPattern, URLResolver
 
 from . import views
 
 app_name: str = 'users'
-urlpatterns: List[URLResolver or URLPattern] = [
+urlpatterns: List[Union[URLPattern, URLResolver]] = [
     # Include default django authentication URLs.
     path('', include('django.contrib.auth.urls')),
     # Registration page.
