d7887f4b984ebb40fab72bce22a7b55d5504fd3c
rpshende <rpshende@DESKTOP-VK57T7M>
2020-12-30 15:45:38 -0500

    Implement password reset feature, create new url patterns and create templates

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,8 @@
 """Defines URL patterns for `users` app."""
 from typing import List, Union
 
-from django.urls import include, path
+from django.contrib.auth import views as auth_views
+from django.urls import include, path, reverse_lazy
 from django.urls.resolvers import URLPattern, URLResolver
 
 from . import views
@@ -13,5 +14,21 @@ urlpatterns: List[Union[URLPattern, URLResolver]] = [
     # Registration page.
     path('register/', views.register, name='register'),
     path('activate/<slug:uidb64>/<slug:token>/',
-         views.activate, name='activate')
+         views.activate, name='activate'),
+    path('reset_password/', auth_views.PasswordResetView.as_view(
+        template_name='registration/password_reset_form.html',
+        email_template_name='registration/password_reset_email.html',
+        subject_template_name='registration/password_reset_subject.txt',
+        success_url=reverse_lazy('users:password_reset_done')
+    ), name='reset_password'),
+    path('reset_password/done/', auth_views.PasswordResetDoneView.as_view(
+        template_name='registration/password_reset_done.html'
+    ), name='reset_password_done'),
+    path('reset_password/confirm/<slug:uidb64>/<slug:token>/',
+         auth_views.PasswordResetConfirmView.as_view(
+             template_name='registration/password_reset_confirm.html',
+             success_url=reverse_lazy('users:reset_password_complete')
+         ), name='reset_password_confirm'),
+    path('reset_password/complete/', auth_views.PasswordResetCompleteView.as_view(),
+         name='reset_password_complete'),
 ]
