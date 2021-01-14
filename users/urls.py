c9aa177544a975aed08a403e462beedabbf0692e
rpshende <rpshende@DESKTOP-VK57T7M>
2021-01-14 15:01:11 -0500

    Add feature for user to delete account.

--- users/urls.py
+++ users/urls.py
@@ -2,8 +2,7 @@
 from typing import List, Union
 
 from django.contrib.auth import views as auth_views
-from django.urls import include, path, reverse_lazy
-from django.urls.resolvers import URLPattern, URLResolver
+from django.urls import URLPattern, URLResolver, include, path, reverse_lazy
 
 from . import views
 
@@ -15,20 +14,27 @@ urlpatterns: List[Union[URLPattern, URLResolver]] = [
     path('register/', views.register, name='register'),
     path('activate/<slug:uidb64>/<slug:token>/',
          views.activate, name='activate'),
+    # Reset password page
     path('reset_password/', auth_views.PasswordResetView.as_view(
         template_name='registration/password_reset_form.html',
         email_template_name='registration/password_reset_email.html',
         subject_template_name='registration/password_reset_subject.txt',
         success_url=reverse_lazy('users:password_reset_done')
     ), name='reset_password'),
+    # Reset password email sent.
     path('reset_password/done/', auth_views.PasswordResetDoneView.as_view(
         template_name='registration/password_reset_done.html'
     ), name='reset_password_done'),
+    # Confirm password reset. Enter new passwrod.
     path('reset_password/confirm/<slug:uidb64>/<slug:token>/',
          auth_views.PasswordResetConfirmView.as_view(
              template_name='registration/password_reset_confirm.html',
              success_url=reverse_lazy('users:reset_password_complete')
          ), name='reset_password_confirm'),
+    # Password reset complete. Password changed.
     path('reset_password/complete/', auth_views.PasswordResetCompleteView.as_view(),
          name='reset_password_complete'),
+    # Delete a user's account.
+    path('delete_account/<pk>',
+         views.AccountDeleteView.as_view(), name='delete_account')
 ]
--- users/views.py
+++ users/views.py
@@ -1,6 +1,7 @@
-from typing import Union
+from typing import Optional, Union
 
 from django.contrib.auth import login
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.contrib.sites.shortcuts import get_current_site
 from django.core.handlers.wsgi import WSGIRequest
 from django.core.mail import EmailMessage
@@ -10,6 +11,7 @@ from django.shortcuts import get_object_or_404, redirect, render
 from django.template.loader import render_to_string
 from django.utils.encoding import force_bytes, force_text
 from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
+from django.views.generic import DeleteView
 
 from .forms import CustomUserCreationForm
 from .models import CustomUser
@@ -78,3 +80,12 @@ def activate(request: WSGIRequest, uidb64: bytes, token: str) -> Union[HttpRespo
         return redirect('learning_logs:index')
     else:
         return render(request, 'registration/invalid_link.html')
+
+
+class AccountDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = CustomUser
+    success_url = '/'
+    template_name = 'registration/delete_account.html'
+
+    def test_func(self) -> Optional[bool]:
+        return self.get_object() == self.request.user
